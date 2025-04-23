4b34b95eef8d8fe3685dc5bbcd19357c7cf821a5
Kochkina Ekaterina <gudrum1983@gmail.com>
2025-04-23 17:34:32 +0200

    вторая попытка настройки деплоя

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,15 +4,19 @@ import CreateTask from "../pages/createTask/createTask.tsx";
 import EditTask from "../pages/editTask/editTask.tsx";
 import styles from "./app.module.scss";
 
+const baseUrl = process.env.VITE_BASE_URL || '/';
+
+console.log("baseUrl", baseUrl);
+
 
 function App() {
   return (
     <div className={styles.app}>
       <Routes>
-        <Route path="/" element=<Home/>/>
-        <Route path="/create" element=<CreateTask/>/>
-        <Route path="/:id" element=<EditTask/>/>
-        <Route path="/*" element=<Home/>/>
+        <Route path={baseUrl} element=<Home/>/>
+        <Route path={`${baseUrl}create`} element=<CreateTask/>/>
+        <Route path={`${baseUrl}:id`} element=<EditTask/>/>
+        <Route path={`${baseUrl}*`} element=<Home/>/>
       </Routes>
     </div>
   )
